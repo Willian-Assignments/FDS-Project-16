1c2e48a7b4ebde521d25ef1c18eac359ec1ba308
Puriney <youryanyun@gmail.com>
2016-12-12 01:13:47 -0500

    modify

--- src/get_data.sh
+++ src/get_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ../data
 wget https://s3.amazonaws.com/tripdata/201501-citibike-tripdata.zip
-unzip *zip
+# wget https://s3.amazonaws.com/tripdata/201506-citibike-tripdata.zip
+# wget https://s3.amazonaws.com/tripdata/201510-citibike-tripdata.zip
+ for z in *.zip; do unzip $z; done
